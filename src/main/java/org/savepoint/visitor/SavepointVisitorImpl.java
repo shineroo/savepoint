e974beb9e821505951682ee8e62116260a8e8754
goblin <gustonas1@gmail.com>
2022-05-05 18:02:04 +0300

    print(expression) works with integers
    it does

--- src/main/java/org/savepoint/visitor/SavepointVisitorImpl.java
+++ src/main/java/org/savepoint/visitor/SavepointVisitorImpl.java
@@ -2,4 +2,23 @@ package org.savepoint.visitor;
 
 public class SavepointVisitorImpl extends SavepointBaseVisitor<Object> {
     //TODO implement
+    @Override
+    public Object visitPrintFunctionCall(SavepointParser.PrintFunctionCallContext ctx) {
+        String text = visit(ctx.expression()).toString();
+        System.out.println(text);
+        return null;
+    }
+
+    @Override
+    public Object visitConstantExpression(SavepointParser.ConstantExpressionContext ctx) {
+        return visit(ctx.constant());
+    }
+
+    @Override
+    public Object visitConstant(SavepointParser.ConstantContext ctx) {
+        if (ctx.INTEGER() != null) {
+            return Integer.parseInt(ctx.INTEGER().getText());
+        }
+        return null;
+    }
 }
